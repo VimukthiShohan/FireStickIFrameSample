6506458f97e44837a293bbad195e757de57466aa
Vimukthi Shohan <vimukthi.shohan@gmail.com>
2023-02-07 12:28:55 +0530

    Network access issue fixed

--- app/src/main/java/com/example/firestickiframesample/MainActivity.java
+++ app/src/main/java/com/example/firestickiframesample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.firestickiframesample;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -8,15 +10,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity {
-    private WebView webView;
+    WebView webView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://www.google.com");
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new Callback());
+//        webView.loadUrl("http://www.SandipBhattacharya.com");
+//        webView.loadUrl("http://www.google.com");
+        webView.loadUrl("https://app.stec.tv/display-login");
+//        webView.loadUrl("https://app.stec.tv/");
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
     }
 }
